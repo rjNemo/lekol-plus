4c6fa32dfb1e1eacef31732c6b599c1e138b92f0
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-07-15 19:56:27 +0200

    🧹 clean boilerplate

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,6 +2,8 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import Logo from "./logo"
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -24,6 +26,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
+          <Logo />
           {siteTitle}
         </Link>
       </h1>
---
+++ src/components/logo.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Logo = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "lekol-plus-Logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Img
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      style={{ height: "2rem", width: "2rem" }}
+    />
+  )
+}
+
+export default Logo
