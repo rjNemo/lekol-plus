44630004fc326f0b188fce6f205e72ebb5d3307b
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-07-16 11:35:23 +0200

    🩸 lay about section out

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
+import About from "../components/about"
 
 const IndexPage = () => (
   <Layout>
@@ -11,6 +12,7 @@ const IndexPage = () => (
       title="Le PLUS pour mener votre enfant vers la réussite"
       subtitle="Accompagnement et suivi personnalisé orienté vers l'international"
     />
+    <About />
   </Layout>
 )
 
